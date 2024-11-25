2303c5c7eda5f63f67de098acbc1ed0659c97028
andrea-altamirano <159966662+andrea-altamirano@users.noreply.github.com>
2024-11-25 23:48:10 +0000

    SpaceInvadrers 0.5

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+#include <math.h>
+#include <string>
+const int X = 10;
+cont Y = 10;
+using namespace std;
+    struct hero
+    {
+        char code; //206 
+        
+    }
+
+int main()
+{
+    char world [X] [Y];
+
+    return 0;
+}
